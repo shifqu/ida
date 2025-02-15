6d6a6350fde4bde9a3510d6cc6dad71e5a22d5d6
Sonny V <sonny@softllama.net>
2025-02-15 14:55:03 +0100

    use UUIDField as a default_auto_field

--- src/ida/settings.py
+++ src/ida/settings.py
@@ -119,4 +119,4 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = "django.db.models.UUIDField"
