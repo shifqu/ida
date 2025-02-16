a2a2230c77b3453e2c53784ac99b1d8cbd3c8676
Sonny V <sonny@softllama.net>
2025-02-16 11:45:01 +0100

    enable login required for all views by using LoginRequiredMiddleware

--- src/ida/settings.py
+++ src/ida/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.LoginRequiredMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
@@ -120,3 +121,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Login and redirect urls
+# https://docs.djangoproject.com/en/5.1/ref/settings/#login-redirect-url
+LOGIN_REDIRECT_URL = "home"
+LOGIN_URL = "login"
+LOGOUT_REDIRECT_URL = "login"
