1ddf59dd30056b70bd51a0d8eff1732e705d3706
Sonny V <sonny@softllama.net>
2025-02-28 15:48:12 +0100

    companies: add bank account and use verbose name

--- src/apps/companies/admin.py
+++ src/apps/companies/admin.py
@@ -2,14 +2,21 @@
 
 from django.contrib import admin
 
-from apps.companies.models import Company
+from apps.companies.models import BankAccount, Company
 from apps.geo.admin import AddressInline
+
+
+class BankAccountInline(admin.TabularInline):
+    """Represent the Bank Account inline to add/remove bank accounts."""
+
+    model = BankAccount
+    extra = 0
 
 
 class CompanyAdmin(admin.ModelAdmin):
     """Represent the Company admin."""
 
-    inlines = [AddressInline]
+    inlines = [AddressInline, BankAccountInline]
 
 
 admin.site.register(Company, CompanyAdmin)
--- src/apps/companies/models.py
+++ src/apps/companies/models.py
@@ -1,5 +1,7 @@
 """Companies models."""
 
+from typing import TYPE_CHECKING
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -7,15 +9,29 @@ from django.utils.translation import gettext_lazy as _
 class Company(models.Model):
     """Represents a company."""
 
-    name = models.CharField(max_length=255)
+    if TYPE_CHECKING:
+        from apps.geo.models import Address
+
+        address_set: models.Manager["Address"]
+        bankaccount_set: models.Manager["BankAccount"]
+
+    name = models.CharField(verbose_name=_("name"), max_length=255)
     phone = models.CharField(
-        max_length=255, blank=True, default="", help_text=_("Phone number with country code (eg. +32 490 12 34 56)")
+        verbose_name=_("phone"),
+        max_length=255,
+        blank=True,
+        default="",
+        help_text=_("Phone number with country code (eg. +32 490 12 34 56)"),
     )
-    email = models.EmailField(max_length=255, blank=True, default="")
-    website = models.URLField(max_length=255, blank=True, default="")
-    vat_number = models.CharField(max_length=255, blank=True, default="", help_text=_("VAT number"))
+    email = models.EmailField(verbose_name=_("email"), max_length=255, blank=True, default="")
+    website = models.URLField(verbose_name=_("website"), max_length=255, blank=True, default="")
+    vat_number = models.CharField(verbose_name=_("VAT number"), max_length=255, blank=True, default="")
     business_court = models.CharField(
-        max_length=255, blank=True, default="", help_text=_("Business court (eg. Antwerpen, afd. Hasselt)")
+        verbose_name=_("business court"),
+        max_length=255,
+        blank=True,
+        default="",
+        help_text=_("Example: Antwerpen, afd. Hasselt"),
     )
 
     def __str__(self):
@@ -30,17 +46,24 @@ class Company(models.Model):
     class Meta:
         """Add a correct plural name."""
 
-        verbose_name_plural = "Companies"
+        verbose_name = _("company")
+        verbose_name_plural = _("companies")
 
 
 class BankAccount(models.Model):
     """Represent a bank account."""
 
-    iban = models.CharField(max_length=34)
-    bic = models.CharField(max_length=11, blank=True, default="")
-    name = models.CharField(max_length=255, blank=True, default="")
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    iban = models.CharField(verbose_name=_("iban"), max_length=34)
+    bic = models.CharField(verbose_name=_("bic"), max_length=11, blank=True, default="")
+    name = models.CharField(verbose_name=_("name"), max_length=255, blank=True, default="")
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name=_("company"))
 
     def __str__(self):
         """Return the string representation of the bank account."""
         return f"{self.name} {self.iban}"
+
+    class Meta:
+        """Set meta options."""
+
+        verbose_name = _("bank account")
+        verbose_name_plural = _("bank accounts")
