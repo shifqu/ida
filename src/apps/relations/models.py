f3803711f7d0cb889eac5d336d49495a84c5902a
Sonny V <sonny@softllama.net>
2025-02-28 15:50:38 +0100

    relations: add verbose_names and get_vat_display

--- src/apps/relations/models.py
+++ src/apps/relations/models.py
@@ -1,5 +1,7 @@
 """Relations models."""
 
+from typing import TYPE_CHECKING
+
 from django.conf.global_settings import LANGUAGES
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -8,23 +10,47 @@ from django.utils.translation import gettext_lazy as _
 class Relation(models.Model):
     """Represent a relation."""
 
+    if TYPE_CHECKING:
+        from apps.geo.models import Address
+
+        address_set: models.Manager["Address"]
+
     class Category(models.TextChoices):
         """Represent the relation type choices."""
 
         CUSTOMER = "CUSTOMER", _("Customer")
         SUPPLIER = "SUPPLIER", _("Supplier")
 
-    name = models.CharField(max_length=255)
-    category = models.CharField(max_length=50, choices=Category.choices)
-    language = models.CharField(max_length=10, default="en", choices=LANGUAGES)
+    name = models.CharField(verbose_name=_("name"), max_length=255)
+    category = models.CharField(verbose_name=_("category"), max_length=50, choices=Category.choices)
+    language = models.CharField(verbose_name=_("language"), max_length=10, default="en", choices=LANGUAGES)
     phone = models.CharField(
-        max_length=255, blank=True, default="", help_text=_("Phone number with country code (eg. +32 490 12 34 56)")
+        verbose_name=_("phone"),
+        max_length=255,
+        blank=True,
+        default="",
+        help_text=_("Phone number with country code (eg. +32 490 12 34 56)"),
     )
-    email = models.EmailField(max_length=255, blank=True, default="")
-    website = models.URLField(max_length=255, blank=True, default="")
-    vat_number = models.CharField(max_length=255, blank=True, default="")
-    bank_account_number = models.CharField(max_length=255, blank=True, default="")
+    email = models.EmailField(verbose_name=_("email"), max_length=255, blank=True, default="")
+    website = models.URLField(verbose_name=_("website"), max_length=255, blank=True, default="")
+    vat_number = models.CharField(
+        verbose_name=_("VAT number"), max_length=255, blank=True, default="", help_text=_("VAT number")
+    )
+    bank_account_number = models.CharField(
+        verbose_name=_("bank account number"), max_length=255, blank=True, default=""
+    )
 
     def __str__(self):
         """Return the string representation of the relation."""
         return self.name
+
+    def get_vat_number_display(self):
+        """Return the VAT number for display."""
+        vat_str = _("VAT")
+        return f"{vat_str} {self.vat_number}"
+
+    class Meta:
+        """Set meta options."""
+
+        verbose_name = _("relation")
+        verbose_name_plural = _("relations")
